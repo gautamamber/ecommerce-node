0aa0f7dd2a4b1a304eda20f68d5f2506c3746c90
gautamamber <ambergautam1@gmail.com>
2021-06-19 13:31:21 +0530

    feat(): add product model

---
+++ backend/models/product.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const {ObjectId} = mongoose.Schema;
+
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    description: {
+        type: String,
+        trim: false,
+        required: true,
+        maxlength: 200
+    },
+    price: {
+        type: Number,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    // link category
+    category: {
+        type: ObjectId,
+        ref: "Category",
+        required: true
+    },
+
+    stock: {
+        type: Number
+    },
+
+    sold: {
+        type: Number,
+        default: 0
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    }
+}, {timestamp:true})
+
+module.exports = mongoose.model("Product", productSchema);
