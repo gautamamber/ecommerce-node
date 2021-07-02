692f9985fb707e16c9065a249e462658f194e259
gautamamber <ambergautam1@gmail.com>
2021-07-02 14:53:33 +0530

    feat(): add user and order update roures

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-
+const Order = require("../models/order");
 
 exports.getUserByID = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -21,3 +21,53 @@ exports.getUser = (req, res) => {
     return res.json(req.profile)
 }
 
+
+exports.getAllUsers = (req, res) => {
+    User.find().exec((err, users) => {
+        if (err || !users) {
+            return res.status(400).json({
+                error: "No users"
+            })
+        }
+
+        res.json(users);
+    })
+}
+
+exports.updateUser = (req, res) => {
+    // we have to firing the update query
+    User.findByIdAndUpdate(
+        {
+            _id: req.profile._id
+        },
+        {$set: req.body},
+        {new: true, useFindAndModify: false},
+        (err, user) => {
+            if(err) {
+                return res.status(400).json({
+                    error: "Update failed"
+                })
+            }
+
+            res.json(user)
+        
+        }
+    )
+}
+
+
+exports.userPurchaseList = (req, res) => {
+    Order.find({user: user.profile._id})
+    .populate("user", "_id name email")
+    .exec((err, order) => {
+        if (err) {
+            return res.status(400).json(
+                {
+                    error: "No order for this user"
+                }
+            )
+        }
+        return res.json(order);
+    })
+
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,14 +2,18 @@ const express = require("express");
 
 const router = express.Router()
 
-const { getUserByID, getUser } = require("../controllers/user");
-const {isAdmin, isAuthenticated, isSignedIn} = require("../controllers/auth");
+const { getUserByID, getUser, updateUser, userPurchaseList } = require("../controllers/user");
+const {isAdmin, isAuthenticated, isSignedIn, getAllUsers} = require("../controllers/auth");
 
 router.param("userId", getUserByID);
 
 
 router.get("/user/:userId", isAuthenticated, isSignedIn, getUser);
 
+router.get('/users', getAllUsers);
 
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+
+router.get('/order/user/:userId', isSignedIn, isAuthenticated, userPurchaseList);
 
 module.exports = router
